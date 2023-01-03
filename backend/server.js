bd54e160b4fdde021adaa09b6eae5eb2b9fe3c4b
Eraze86 <therese.rosvall@hotmail.se>
2023-01-03 12:56:02 +0100

    express

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000, function(){
-    console.log("servern är igång")
-});
-app.get("/", function(req, res){
-res.send("<h2>Hejsan</h2>")
-})
