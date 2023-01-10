c7bce46ef172b77165f96ef08a6d548a77294a42
Eraze86 <therese.rosvall@hotmail.se>
2023-01-10 13:24:04 +0100

    added forms in coaching

--- frontend/src/Components/Contact.tsx
+++ frontend/src/Components/Contact.tsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 export function Contact() {
 
     const [form, setForm] = useState("")
-    
+
 function sendMail(){
 
 }
     return (<>
         <section>
             <h1>Kontakta mig</h1>
-            <span>Har ni några frågor så skicka gärna ett mail till mig. </span>
+            <span>Har ni några frågor så skicka gärna ett mail till mig. </span><br/><br/>
             <form>
                 <label>Namn:</label><br/>
                 <input className="border w-full" placeholder="John Doe"></input><br/>
--- frontend/src/Components/Private.tsx
+++ frontend/src/Components/Private.tsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react"
 export function Private() {
 
     const [price, setPrice] = useState(500)
+    const [ form, setForm] = useState("")
+    function sendMail(){
+
+    }
     // useEffect(() => {
     //     fetch("privatecours.json")
       
@@ -15,7 +19,7 @@ export function Private() {
     // },[])
     return (<>
     <section><h1>Privatcoaching</h1>
-
+<article>
         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -23,11 +27,25 @@ export function Private() {
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/>
              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam'
-
-             </span>
-             <p className="font-medium">Pris: {price}</p>
-             <button>Boka här</button>
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam<br/><br/>
+            </span>
+             <p className="font-medium">Pris: {price} kr/timmen</p><br/>
+     
+             </article>
+             <article>
+                <p>Fyll i uppgifterna nedanför och berätta lite vad du är intresserad av, så kommer jag att kontakta dig.</p><br/>
+            <form>
+                <label>Namn:</label><br/>
+                <input className="border w-full" placeholder="John Doe"></input><br/>
+                <label>E-mail:</label><br/>
+                <input className="border w-full" placeholder="johnDoe@hotmail.se"></input><br/>
+                <label>Telefonnr:</label><br/>
+                <input className="border w-full" placeholder="070x-xxxxxx"></input><br/>
+                <label>Meddelande</label><br/>
+                <textarea className="border w-full h-36"  placeholder="Skriv vad du är intresserad av här"></textarea><br/>
+                <button onClick={sendMail}>Skicka</button>
+            </form>
+            </article>
              </section>
              </>)
 }
