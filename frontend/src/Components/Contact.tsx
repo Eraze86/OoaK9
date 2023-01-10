b0ab141e1e5ccc72d1577599abb24a00f5220901
Eraze86 <therese.rosvall@hotmail.se>
2023-01-10 13:03:49 +0100

    create a form in contact

--- frontend/src/Components/Contact.tsx
+++ frontend/src/Components/Contact.tsx
@@ -1,4 +1,27 @@
-export function Contact(){
+import { useState } from "react"
 
-    return(<>Contacts</>)
+export function Contact() {
+
+    const [form, setForm] = useState("")
+    
+function sendMail(){
+
+}
+    return (<>
+        <section>
+            <h1>Kontakta mig</h1>
+            <span>Har ni några frågor så skicka gärna ett mail till mig. </span>
+            <form>
+                <label>Namn:</label><br/>
+                <input className="border w-full" placeholder="John Doe"></input><br/>
+                <label>E-mail:</label><br/>
+                <input className="border w-full" placeholder="johnDoe@hotmail.se"></input><br/>
+                <label>Telefonnr:</label><br/>
+                <input className="border w-full" placeholder="070x-xxxxxx"></input><br/>
+                <label>Meddelande</label><br/>
+                <textarea className="border w-full h-36"  placeholder="Skriv ett medelande här"></textarea><br/>
+                <button onClick={sendMail}>Skicka</button>
+            </form>
+        </section>
+    </>)
 }
