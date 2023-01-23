69d3fdfe8b0aa4bef65588899cdedd99fcdeb167
Eraze86 <therese.rosvall@hotmail.se>
2023-01-23 18:10:16 +0100

    select option choose date

--- frontend/src/Components/Book-course.tsx
+++ frontend/src/Components/Book-course.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, FormEventHandler, useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { ICourses } from "./module/ICourses"
 import courseImg from "../img/11.jpg"
@@ -16,7 +16,7 @@ export function BookCourse() {
     const [courses, setCourses] = useState<ICourses[]>([])
     const [courseId, setCourseId] = useState(0)
     const [gdpr, setGdpr] = useState(false)
-    const [chooseDate, setChooseDate] = useState("")
+    // const [chooseDate, setChooseDate] = useState("")
     const [bookCourse, setBookCourse] = useState<IBookCourse[]>([])
 
     let params = useParams();
@@ -32,7 +32,11 @@ export function BookCourse() {
             setCourses(JSON.parse(local))
         }
     }, []);
-
+function handleDate( e:any ){
+    let uppdate = ({ ...bookCourse, date: e})
+    setBookCourse(uppdate)
+    console.log("eller?", uppdate)
+}
 
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         let name = e.target.name
@@ -40,11 +44,15 @@ export function BookCourse() {
         setBookCourse(uppdate)
     }
     function sendBooking() {
-axios.post<IBookCourse>
+// axios.post<IBookCourse>("http://localhost:3001/courses")
+// .then((response) => {    
+//    console.log(response.data)
+
+// })   
 
         console.log("skicka boking", bookCourse)
     }
-console.log(chooseDate)
+
     //map out courses, id id match print the right cours
     //the map out dates, add clickbutton to show dates when clicked
     let showBooking = courses.map((course: ICourses) => {
@@ -57,18 +65,16 @@ console.log(chooseDate)
                         <h5 className="">Beskrivning:</h5> <h6>{course.description} </h6>
                         <h5 className="py-2">{course.price} kr</h5>
                         <div className="mb-8">
-                            <h5 className="py-2">Valbara datum:</h5>
-                                    <ul className="w-full">
-
+                        <h5 className="py-2">Lediga datum</h5>
+                                    <select className="border w-full"  onChange={(e) => {handleDate(e.target.value)}} >
                                         {course.dates.map((d, i: number) =>
-                                            <li key={i} className=" border my-1 p-2 rounded hover:bg-primary flex " onClick={() => setChooseDate(d.date)} >
-                                                    <p>{d.date},</p>
-                                                    <p className="ml-2"> Platser kvar: {d.number } </p>
-                                            </li>
+                                            <option key={i}  value={d.date} className="mx-2">
+                                                    {d.date},
+                                                    Platser kvar: {d.number }
+                                            </option>
                                         )}
-
-                                    </ul>
-                                    
+                                        </select>
+                                          
                             </div>
                         </div>
 
@@ -81,11 +87,11 @@ console.log(chooseDate)
     return (<>
         <main className="lg:flex lg:mx-24">
 
-            <section className="lg:m-12 lg:w-3/6">
+            <section className="w-5/6 lg:m-12 lg:w-3/6">
                 <h1 className="mt-0">Bokning</h1>
                 {showBooking}
                 <article>
-                    <div className="">
+                    <div className="w-full">
                         <form className="flex flex-col">
                             <label>Ägare:</label>
                             <input name="name" onChange={handleChange} />
@@ -98,7 +104,7 @@ console.log(chooseDate)
                             <label>Ålder på hunden:</label>
                             <input name="age" onChange={handleChange} />
                             <label>Medelande.</label>
-                            <input className="h-48" type="textarea" name="messenge" onChange={handleChange} />
+                            <input className="h-48 align-top" type="textarea" name="messenge" onChange={handleChange} />
                             <div className="flex items-center" >
                                 <label>Godkänner Gdpr</label>
                                 <input type="checkbox" name="gdpr" className="w-4 h-4 mx-4" onClick={() => setGdpr(!gdpr)} />
