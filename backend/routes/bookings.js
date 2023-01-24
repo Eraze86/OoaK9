04b07f3be06d9092037a3c6a1c2631ae1f1d164d
Eraze86 <therese.rosvall@hotmail.se>
2023-01-24 14:48:39 +0100

    get bookings, print bookings, tailwind

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-
+const bookings = require("../bookings.json") 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+    res.send(bookings)
 });
 
 module.exports = router;
--- frontend/src/Components/admin/Booking.tsx
+++ frontend/src/Components/admin/Booking.tsx
@@ -1,3 +1,53 @@
-export function Booking(){
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { IBookCourse } from "../module/IBookCourse"
 
-    return(<>Bokning</>)}
+export function Booking(){
+const [bookings, setBookings]= useState<IBookCourse[]>([])
+const [info, setInfo] = useState(false)
+    useEffect(() => {
+      
+        axios.get<IBookCourse[]>("http://localhost:3001/bookings")
+        .then((response) => {    
+            setBookings(response.data) 
+            console.log(response.data)
+          
+        })   
+}, [])
+let printBookings = bookings.map((book: IBookCourse, i: number) => {
+return(<><tr key={i} className="bg w-full odd:bg-[#E8E8E8] even:bg-[#DBDBDB]">
+    <td>{book.course}</td>
+    <td>{book.name}</td>
+    <td>{book.phone}</td>
+    <td>{book.date}</td>
+
+    <td><button className="bg-inherit w-8 "onClick={() => setInfo(!info)}>V</button></td> 
+    
+    </tr>
+  
+    {info && <tr className="mb-2 w-11/12 bg-[#E8E8E8] flex justify-between">
+    <td>Mail: {book.mail}</td>
+    <td>Ras:{book.breed}</td>
+    <td>Ålder:{book.age}</td>
+    <td>{book.messenge}</td>
+    
+    <div><td><a className="w-16 mr-2">Ändra</a><a className="w-8 mr-2 text-red text-center">X</a></td></div></tr>}
+    </> )
+   
+})
+    return(<>
+    <section >
+    <table className="table-auto w-full" >
+        <tbody className=" w-full">
+    <tr >
+    <th>Kurs</th>
+    <th>Namn</th>
+    <th>Telefon</th>
+    <th>Datum</th>
+
+   
+  </tr>
+    {printBookings}
+    </tbody>
+    </table>
+        </section></>)}
